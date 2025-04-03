92825af3b8f5b8f8e5833435c339b5bdf34f76a7
Akhilesh Kanbarkar <86067465+Akhilesh-Kanbarkar@users.noreply.github.com>
2025-04-02 22:16:35 -0400

    Revert "Secured XMLExporter against XXE attacks."

--- jme3-plugins/src/xml/java/com/jme3/export/xml/XMLExporter.java
+++ jme3-plugins/src/xml/java/com/jme3/export/xml/XMLExporter.java
@@ -79,13 +79,7 @@ public class XMLExporter implements JmeExporter {
     public void save(Savable object, OutputStream outputStream) throws IOException {
         Document document = null;
         try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            // XXE Protection for Document Builder
-            dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-            dbf.setXIncludeAware(false);
-            dbf.setExpandEntityReferences(false);
-
-            document = dbf.newDocumentBuilder().newDocument();
+            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
         } catch (ParserConfigurationException ex) {
             throw new IOException(ex);
         }
